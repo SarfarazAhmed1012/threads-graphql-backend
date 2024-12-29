46b3c1ee72859fc3ff74c88193950bc9c778d876
SarfarazAhmed1012 <sarfarazahmed1012@gmail.com>
2024-12-29 21:32:48 +0500

    implemented signin and signup functionaility

--- src/graphql/user/resolvers.ts
+++ src/graphql/user/resolvers.ts
@@ -1,8 +1,19 @@
-const queries = {};
+import UserService, { CreateUserPayload } from "../../services/user";
+
+const queries = {
+  getUserToken: async (
+    _: any,
+    payload: { email: string; password: string }
+  ) => {
+    const res = await UserService.getUserToken(payload);
+    return res;
+  },
+};
 
 const mutations = {
-  createUser: async (_: any, {}, {}) => {
-    return "random string";
+  createUser: async (_: any, payload: CreateUserPayload) => {
+    const res = await UserService.createUser(payload);
+    return res.id;
   },
 };
 
---
+++ src/services/user.ts
@@ -0,0 +1,70 @@
+import { createHmac, randomBytes } from "crypto";
+import { prismaClient } from "../lib/db";
+import JWT from "jsonwebtoken";
+
+const JWT_SECRET = "mysecretkey";
+export interface CreateUserPayload {
+  firstName: string;
+  lastName?: string;
+  email: string;
+  password: string;
+}
+
+export interface UserTokenPayload {
+  email: string;
+  password: string;
+}
+
+class UserService {
+  public static createUser(payload: CreateUserPayload) {
+    const { firstName, lastName, email, password } = payload;
+    const salt = randomBytes(32).toString("hex");
+    const hashedPassword = UserService.generateHash(salt, password);
+    return prismaClient.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+        salt,
+      },
+    });
+  }
+
+  private static generateHash(salt: string, password: string) {
+    return createHmac("sha256", salt).update(password).digest("hex");
+  }
+
+  private static async getUserByEmail(email: string) {
+    return prismaClient.user.findUnique({ where: { email } });
+  }
+
+  public static async getUserToken(payload: UserTokenPayload) {
+    const { email, password } = payload;
+    const user = await UserService.getUserByEmail(email);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+    const salt = user.salt;
+    const hashedPassword = UserService.generateHash(salt, password);
+
+    if (hashedPassword !== user.password) {
+      throw new Error("Invalid password");
+    }
+
+    // get token
+    const token = JWT.sign(
+      {
+        email: user.email,
+        id: user.id,
+      },
+      JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    return token;
+  }
+}
+
+export default UserService;
